785b61193b9b58500dd8ecb6988f0b0a78218b5b
hx <beltranbot@gmail.com>
2018-09-30 15:25:17 -0500

    jsx

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const greeting = React.createElement('h1', {}, 'Hello world!')
+const getCurrentDate = () => new Date().toDateString()
+
+// const greeting = React.createElement('h1', {}, 'Hello world!')
+const greeting = (
+    <h1>Hellow Worlds Current date: {getCurrentDate()}</h1>
+)
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(greeting, document.getElementById('root'));
