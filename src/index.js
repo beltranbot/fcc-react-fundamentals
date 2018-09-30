fea8c97e841b6491bcb93ab95a7d8dfb89703d42
hx <beltranbot@gmail.com>
2018-09-30 15:59:10 -0500

    functional compoennets

--- src/index.js
+++ src/index.js
@@ -4,13 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const getCurrentDate = () => new Date().toDateString()
-
-// const greeting = React.createElement('h1', {}, 'Hello world!')
-const greeting = (
-    <h1>Hellow Worlds Current date: {getCurrentDate()}</h1>
-)
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(greeting, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
